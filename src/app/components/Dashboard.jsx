af18a58b541838c08ead87379149c6e9006936ad
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-03-25 10:53:08 -0400

    updated dependencies and added routes

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { ConnectedTaskList } from './TaskList';
+
 export const Dashboard = ({ groups }) => (
   <div>
     <h2>Dashboard</h2>
